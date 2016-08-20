2c725e44f48080442c64a892e1430b88db4875a7
Michael Ryan <mickeyryan1987@gmail.com>
2016-08-20 14:15:57 -0400

    added query url and start ajax request

--- javascript/search.js
+++ javascript/search.js
@@ -1,13 +1,29 @@
 
 
 // variables
-
-var urlEndpoint = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+console.log('test');
+var urlEndpoint = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 // var queryURL = urlEndpoint + apiKey + q;
 
-urlEndpoint += $.param({
-	'api-key':'1b8ad75b08d7499ab6862418e9cc2c3a',
-	'q': $('query'),
-	'begin_date': $('startDate'),
-	'end_date': $('endDate'),
-})
+
+
+
+
+
+$('#clearBtn').click(function(){
+	var q = $('#search').val(),
+		begin_date = $('#startYear').val(),
+		end_date = $('#endYear').val();
+
+		urlEndpoint += $.param({
+			'api-key':'1b8ad75b08d7499ab6862418e9cc2c3a',
+			'q' : q,
+			'begin_date': begin_date,
+			'end_date': end_date
+		});
+
+		$.get(urlEndpoint,function(data){
+			console.log(data);
+		});
+	console.log(urlEndpoint);
+});
