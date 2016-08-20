d717c57e823d62b3b9ad01cc69368e1bb205399f
John-Mike <jmmarquardt@gmail.com>
2016-08-20 14:01:31 -0400

    updated some js to write headline and abstract

--- javascript/search.js
+++ javascript/search.js
@@ -7,7 +7,36 @@ var urlEndpoint = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
 
 urlEndpoint += $.param({
 	'api-key':'1b8ad75b08d7499ab6862418e9cc2c3a',
-	'q': $('query'),
-	'begin_date': $('startDate'),
-	'end_date': $('endDate'),
-})
+	'q': $('#search'),
+	'begin_date': $('#startYear'),
+	'end_date': $('#endYear'),
+});
+
+// helper write function
+var write = function (element, input) {
+	$(element).html(input);
+};
+
+// submit button handler
+$('#searchBtn').on('click', function () {
+	// ajax call
+	$.ajax({
+		url: urlEndpoint,
+		method: 'GET',
+	})
+	// when info retrieved 
+	.done(function (response) {
+		// variables
+		for (var i = 0; i < 8; i++) {
+			
+			var headline = response.response.docs.headline.main;
+			var abstract = response.reaponse.docs.keywords.snippet;
+			write('#wellSection', '<div>' + headline + '</div>');
+			write('#wellSection', '<div>' + abstract + '</div>');
+		}
+
+		// doc writes
+	});
+});
+
+
